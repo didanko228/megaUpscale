42a6ce43fc611d0e7d4a787f5d8dd31e499d2d84
”didanko228” <artemdidanov072@gmail.com>
2025-12-30 12:44:26 +0300

    add OperatingSystemDetector.java

--- src/main/java/com/didanko228/megaUpscale/Main.java
+++ src/main/java/com/didanko228/megaUpscale/Main.java
@@ -2,6 +2,7 @@ package com.didanko228.megaUpscale;
 
 import com.didanko228.megaUpscale.utils.Config;
 import com.didanko228.megaUpscale.utils.Logger;
+import com.didanko228.megaUpscale.utils.OperatingSystemDetector;
 import com.didanko228.megaUpscale.utils.TranslationManager;
 
 public class Main {
@@ -14,5 +15,6 @@ public class Main {
         Logger.info("KEY: " + Config.KEY);
         Logger.info(TranslationManager.translate("en_us", "helloworld"));
         Logger.info(TranslationManager.translate("ru_ru", "helloworld"));
+        Logger.info("OS: " + OperatingSystemDetector.detectOS());
     }
 }
