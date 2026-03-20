decff98c07cc501e99c0b45bd6be9f3680ba89f3
”didanko228” <51835948+didanko228@users.noreply.github.com>
2026-03-20 15:05:55 +0300

    alpha ui

--- src/main/java/com/didanko228/megaUpscale/Main.java
+++ src/main/java/com/didanko228/megaUpscale/Main.java
@@ -1,18 +1,17 @@
 package com.didanko228.megaUpscale;
 
+import com.didanko228.megaUpscale.ui.MainWindow;
 import com.didanko228.megaUpscale.utils.Logger;
 import com.didanko228.megaUpscale.utils.TranslationManager;
-import com.didanko228.megaUpscale.utils.UpscaleService;
 
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
 
 public class Main {
     public static String PROJECT_NAME = "megaUpscale";
     public static String PROJECT_ID = PROJECT_NAME.toLowerCase();
 
-    static void main() throws Exception {
+    static void main(String[] args) throws Exception {
         TranslationManager.loadTranslations();
 
         // --- первичная загрузка ресурсов ---
@@ -24,10 +23,6 @@ public class Main {
         Logger.info("Backend: " + backend);
         Logger.info("Models: " + modelsDir);
 
-        UpscaleService service = new UpscaleService(backend, modelsDir);
-
-        Path input = Paths.get("./input.jpg");
-        Path output = Paths.get("./output.png");
-        service.upscale(input, output, "4xLSDIR", 4);
+        MainWindow.launchUI(args);
     }
 }
